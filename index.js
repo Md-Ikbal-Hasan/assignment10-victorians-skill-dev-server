745ee71870e7a62fd58ec9e281049c3753e5ad34
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-25 11:09:52 +0600

    load the course api ready

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-
+const course = require('./data/course.json');
 
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('victorians-skill-development server api under working....')
+})
+
+
+// load all the course
+app.get('/course', (req, res) => {
+    res.send(course);
 })
 
 
