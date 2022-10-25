8905cd24947f087c9c53cfd48a2612cea1c5594d
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-25 11:08:17 +0600

    server initilized

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('victorians-skill-development server api under working....')
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log("victorians-skill-development server is running on port ", port);
+})
